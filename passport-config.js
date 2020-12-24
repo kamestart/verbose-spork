8b6431a1566c4fa592b5d8d52f3fe2b7013d6c2d
kamestart <ajmeramanit230734@gmail.com>
2020-12-24 21:05:34 +0530

    First Commit after 4 months

---
+++ passport-config.js
@@ -0,0 +1,41 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+
+
+function initialize(passport, getUserByUsername, getUserById) {
+  const authenticateUser = async (username, password, done) => {
+    const user = await getUserByUsername(username)
+
+    if (user == null) {
+      return done(null, false, { message: 'Incorrect Username / Password 1' })
+    }
+    
+    
+
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user)
+
+      } else {
+          return done(null, false, { message: 'Incorrect Username / Password 2' })
+        } 
+      
+      
+    } catch (e) {
+      console.log(e)
+      return done(e)
+    }
+  }
+
+  passport.use(new LocalStrategy({
+    usernameField: 'email'
+  }, authenticateUser))
+
+  passport.serializeUser((user, done) => done(null, user.id))
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
+}
+
+module.exports = initialize
---
+++ server.js
@@ -0,0 +1,103 @@
+if (process.env.NODE_ENV != 'production') {
+    require('dotenv').config()
+ }
+ const PORT = process.env.PORT || 3000
+ const express = require('express')
+ const app = express()
+ const mongoose = require('mongoose')    
+ 
+ var user_in_db = require('./models/user')
+ const bcrypt = require('bcrypt')
+ const initPassport = require('./passport-config')
+ const passport = require('passport')
+ const flash = require('express-flash')
+ const session = require('express-session')
+ var MemoryStore = require('memorystore')(session)
+ const bodyParser = require('body-parser')
+ var i = 1
+ 
+ initPassport(
+     passport,
+     username => {
+         var useri
+         return useri = user_in_db.findOne( { username: username }).exec()
+         
+     },  
+     id => {
+         var userReturnedById
+         return userReturnedById = user_in_db.findOne({ id: id } ).exec()
+     }
+ 
+ )
+ app.set('trust proxy', 1);
+ 
+ app.listen(PORT, () => {
+     console.log(`Server Started on port ${PORT} `)
+ })
+ 
+ app.get('/create/games', (req, res) => {
+     res.render('create_game')
+ })
+ 
+ app.use(express.static(__dirname + '/public'))
+ app.use(express.urlencoded({ extended: true }))
+ app.use(flash())
+ app.use(session({
+     cookie: {
+             secure: true,
+             maxAge: 86400000
+            },
+     store: new MemoryStore(),
+     secret: process.env.SESSION_SECRET,
+     resave: false,
+     saveUninitialized: false
+ }))
+ app.use(passport.initialize())
+ app.use(passport.session())
+ 
+ mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+ 
+ app.set('view-engine', 'ejs')
+ 
+ app.get('/', (req, res) => {
+     res.render('register.ejs')
+ })
+ 
+ app.get('/login', (req, res) => {
+     res.render('login.ejs')
+ })
+ 
+ app.post('/login', (req, res, next) => {
+     console.log(req.url)
+     passport.authenticate('local', {
+         successRedirect: '/home',
+         failureRedirect: '/login',
+         failureFlash: true
+     })(req, res, next);
+ })
+ 
+ app.post('/', async (req, res) => {
+     try {
+         var salt = await bcrypt.genSalt(16)
+         const hashedPwd = await bcrypt.hash(req.body.password, salt)
+             const newUser = new user_in_db({
+                 username: req.body.username,
+                 email: req.body.email,
+                 password: hashedPwd,
+                 id: i
+             })
+             i = i + 1
+             newUser.save().then(result => {
+                 console.log('erfcfcdwwq3e')
+             })
+             res.redirect('/login') 
+     } catch(e) {
+         console.log(e)
+     }
+ })
+ 
+ app.get('/home', (req, res) => {
+     res.render('index.ejs', { name: req.user.username  })
+ })
+ 
+     
